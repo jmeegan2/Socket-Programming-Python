77768b1e18a09fb5afccd8acd892ea7a6e12d5c2
jmeegan2 <jmeegan8@gmail.com>
2022-12-07 19:38:52 -0500

    code for problem 3 first q

--- echo-server.py
+++ echo-server.py
@@ -1,18 +1,36 @@
-# echo-server.py
+"""
+ Implements a simple HTTP/1.0 Server
+
+"""
 
 import socket
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+# Define socket host and port
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 14000
+
+# Create socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((SERVER_HOST, SERVER_PORT))
+server_socket.listen(1)
+print('Listening on port %s ...' % SERVER_PORT)
+
+while True:    
+   # Wait for client connections
+    client_connection, client_address = server_socket.accept()
+
+    # Get the content of index.html
+    # HTTP Request
+    fin = open('index.html')
+    content = fin.read()
+    fin.close()
+
+    # Send HTTP response
+    response = 'HTTP/1.0 200 OK\n\n' + content
+    client_connection.sendall(response.encode())
+    # client_connection.close()
+
+# Close socket
+server_socket.close()
